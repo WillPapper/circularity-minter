3447683a4f1e52653061375b7312696f0aa34a03
Will Papper <contact@papper.me>
2023-10-26 22:58:37 +0200

    feat: Added the mint of the second NFT (#2)

--- api/burn.ts
+++ api/burn.ts
@@ -7,14 +7,33 @@ const syndicate = new SyndicateClient({
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   try {
-    const tx = await syndicate.transact.sendTransaction({
+    // Burn the Circularity NFT from the prior month, minting the next month in
+    // the process
+    const burnTx = await syndicate.transact.sendTransaction({
       projectId: "2bee69fa-0e96-42cb-a864-8e7258a044a3",
       contractAddress: "0x8680db891ff8c34f2ffcefac43d55059d010a821",
       chainId: 1,
       functionSignature: "burn()",
     });
 
-    res.status(200).send(`Transaction ID Received: ${tx.transactionId}`);
+    // Mint the Circularity Burn Ceremony NFT for the current month
+    // A higher amount could be passed in manually if this falls behind via
+    // query params, but this won't be needed now
+    const mintTx = await syndicate.transact.sendTransaction({
+      projectId: "2bee69fa-0e96-42cb-a864-8e7258a044a3",
+      contractAddress: "0x83cc08E3783E9fF097e87a63BEc7503465C84714",
+      chainId: 1,
+      functionSignature: "mint(uint256 amount)",
+      args: {
+        amount: 1,
+      },
+    });
+
+    res.status(200).send(
+      `<b>Transaction submitted! Transaction IDs Received:</b>
+        <br>Circularity Burn: ${burnTx.transactionId}
+        <br>Circularity Burn Ceremony Mint: ${mintTx.transactionId}`
+    );
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`);
   }
